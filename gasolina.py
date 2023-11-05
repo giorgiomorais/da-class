44891ae64b58de6c57b04c57b7e2bdec01479d8e
giorgiomorais <g.morais_engmec@hotmail.com>
2023-11-05 22:25:20 +0000

    Arquivo gasolina.py criado

---
+++ gasolina.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+gasolina_df = pd.DataFrame(pd.read_csv("gasolina.csv"))
+gasolina_df
+
+# Criando o gráfico de linha para a variação do preço da gasolina
+sns.set(style="whitegrid")     #Definindo o grid branco
+plt.figure(figsize=(8, 6))     # Determinando o tamanho da figura
+sns.lineplot(data=gasolina_df, x='dia', y='venda', marker='o', markersize=8, color='b', label='Preço da Gasolina') # Gerando o gráfico
+plt.xlabel('Dia')     # Definindo os nomes do eixo x
+plt.ylabel('Preço')   # Definindo o nome do eixo y
+
+plt.savefig("gasolina.png")   #Salvando em png
+
+plt.show()            # Mostrando o gráfico
